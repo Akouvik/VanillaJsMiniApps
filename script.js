73368f033caf0e02d589ee44f50efbee1bdf3bd7
Akouvi <kouedjinl@gmail.com>
2018-03-27 15:24:26 -0400

    started adding button to todo items

--- script.js
+++ script.js
@@ -7,28 +7,39 @@ window.onload = function(){
 
 
 }
-
-
-
-
-// function addTodo(){
-//   var button = document.getElementById("button");
-//   button.removeAttribute("data-toggle");
-//   var text = document.createTextNode(item);
-//   var newItem = document.createElement("li");
-//   newItem.appendChild(text);
-//   document.getElementById("todoList").appendChild(newItem);
-// }
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
 function todoList(){
   var item = document.getElementById('todoInput').value;
   var text = document.createTextNode(item);
-  var newItem = document.createElement("li");
-  newItem.appendChild(text);
+  var li = document.createElement("li");
+  li.appendChild(text);
+
   if(item === ''){
     alert('Please enter a todo');
   }else{
-    document.getElementById('todoList').appendChild(newItem);
-
+    document.getElementById('todoList').appendChild(li);
   }
   document.getElementById('todoInput').value = "";
+
+  var span = document.createElement('span');
+  var txt = document.createTextNode('\u00d7');
+  span.className = 'close'
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for(var i=0;i<close.length;i++){
+    close[i].onclick = function(){
+      var div = this.parentElement;
+      div.style.display = 'none';
+    }
+
+  }
 };
